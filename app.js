ae2751c6d50c91a9a0398bbe737ad93ae506b448
jdreyespaez <jd.reyespaez@gmail.com>
2016-10-23 08:55:50 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const ProductList = React.createClass({
+  render: function () {
+    return (
+      <div className='ui items'>
+        Hola, amigo! Soy un componente básico de React.js
+      </div>
+    );
+  },
+});
